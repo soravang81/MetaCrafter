c7ed4e6dff238d67c3a6109f338df21c4d099119
soravang81 <soravang81@gmail.com>
2024-05-23 00:54:54 +0530

    completedJSproof

--- jsproof/meta.js
+++ jsproof/meta.js
@@ -15,17 +15,19 @@ function mintNFT (name , img , nickname , currency , value ) {
 
 function listNFTs () {
     NFTs.forEach((nft)=>{
-        console.log(nft)
+        console.log(
+        "\n\nName : ",nft.name,
+        "\nimg : ",nft.img,
+        "\nnickname : ",nft.nickname,
+        "\ncurrency : ",nft.currency,
+        "\nvalue : ",nft.value)
     })
-
 }
 
 
 function getTotalSupply() {
     console.log("Number of NFTs minted : " , NFTs.length)
 }
-
-
 
 mintNFT("dogecoin" , "blackforest" , "dcx" , "dollar" , 100 );
 mintNFT("dogecoin" , "blackforest" , "dcx" , "dollar" , 100 );
